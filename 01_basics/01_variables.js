d86802abf0fa3d09df157bc74adcdba360375283
AYUSH RANJAN <ayushranjan8757@gmail.com>
2025-09-19 06:58:37 +0000

    this my second file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 8757491266
+let accountEmail = "ayushranjan@8757"
+var accountPassword = "12345" 
+accountCity = "Mathura"
+
+// accountId = 2   [ not allowed ]
+
+accountEmail = "ar@gmail.com"
+accountPassword = "8757"
+accountCity = "Bihar"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
